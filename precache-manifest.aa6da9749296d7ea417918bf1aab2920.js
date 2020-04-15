1e1668763f6f6a2acb49898f57f53c7b5292208e
dimail0880 <dimail4888@gmail.com>
2020-04-15 21:00:59 +0300

    Updates

---
+++ precache-manifest.aa6da9749296d7ea417918bf1aab2920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9887c79206928dd178313c24da7a4af5",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "11b79155122c90203dc2",
+    "url": "/goit-react-hw-02-phonebook/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "f526ed8778c25015c323",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.87e11485.chunk.css"
+  },
+  {
+    "revision": "11b79155122c90203dc2",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.be91476d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.be91476d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f526ed8778c25015c323",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.b49cf83b.chunk.js"
+  },
+  {
+    "revision": "bfccad9aa0892efebfa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.98a8e906.js"
+  }
+]);
